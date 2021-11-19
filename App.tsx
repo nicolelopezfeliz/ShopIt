16b8e255895874daf87f456b8050c2960ed8c307
Joakim Eberholst <J_eberholst@hotmail.com>
2021-11-19 16:19:11 +0100

    * Switched to yarn * Added React Native Paper & implementation * Header.tsx includes Appbar * Default theming added

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import React from 'react';
+import {Appearance, StyleSheet, View} from 'react-native';
 import {DefaultTheme, Provider as PaperProvider} from 'react-native-paper';
 import Header from './src/components/Header';
 import ShoppingList from './src/screens/ShoppingList';
@@ -8,23 +8,21 @@ const theme = {
     ...DefaultTheme,
     colors: {
         ...DefaultTheme.colors,
-        primary: 'tomato',
+        primary: 'green',
         accent: 'yellow',
     },
 };
 
 export default function App() {
 
-  return (
-      <PaperProvider>
-        <View>
-          <Header/>
-          <ShoppingList />
-        </View>
-      </PaperProvider>
-  );
+    return (
+        <PaperProvider theme={theme}>
+            <View>
+                <Header/>
+                <ShoppingList/>
+            </View>
+        </PaperProvider>
+    );
 }
 
-const styles = StyleSheet.create({
-
-});
+const styles = StyleSheet.create({});
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import {View, StyleSheet,Text} from 'react-native';
+import { Appbar } from 'react-native-paper';
 
 const Header = () => {
+
+    const _goBack = () => console.log('Went back');
+    const _handleSearch = () => console.log('Searching');
+    const _handleMore = () => console.log('Shown more');
+
     return (
-        <View style={styles.header}>
-            <Text style={styles.headerTitle}>
-                {"Shopping List"}
-            </Text>
-        </View>
+        <Appbar.Header>
+            <Appbar.BackAction onPress={_goBack} />
+            <Appbar.Content title="Title" subtitle="Subtitle" />
+            <Appbar.Action icon="magnify" onPress={_handleSearch} />
+            <Appbar.Action icon="dots-vertical" onPress={_handleMore} />
+        </Appbar.Header>
     );
 
 }
