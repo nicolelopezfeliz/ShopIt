c575819cc47d92ca6a0e5a9868395360c545b350
Joakim Eberholst <J_eberholst@hotmail.com>
2021-11-20 20:15:09 +0100

    * ShoppingContext.tsx updated with update-item-functionality * FunnyGif.tsx created * Header.tsx got the FunnyGif.tsx implemented

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,29 @@
-import React, {FC} from 'react';
+import React, {FC, useState} from 'react';
 import {StyleSheet} from 'react-native';
 import {Appbar, useTheme} from 'react-native-paper';
 import {useNavigation} from "@react-navigation/native";
+import {FunnyGif} from "./FunnyGif";
 
 const Header: FC = () => {
     const {styles} = useThemedStyles();
     const navigationRef = useNavigation();
+    const [isDisplayingGif, setIsDisplayingGif] = useState(false)
+
+    const toggleGifOnPress = () => {
+        setIsDisplayingGif(!isDisplayingGif)
+    }
 
     return (
         <Appbar.Header style={styles.header}>
             {navigationRef.canGoBack() ? <Appbar.BackAction onPress={navigationRef.goBack}/> : null}
-            <Appbar.Content title="The Blairwitch Shop"/>
+            <Appbar.Content title="The Blairwitch Shop" onPress={toggleGifOnPress}/>
+            <FunnyGif isDisplaying={isDisplayingGif} onPress={() => toggleGifOnPress}/>
         </Appbar.Header>
     );
 }
+
+
+
 
 const useThemedStyles = () => {
     const theme = useTheme();
